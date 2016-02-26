73fc6eed0815e27c45afbf2e795a6c3da87c2de3
miya0001 <miya@wpist.me>
2016-02-26 17:06:19 +0900

    add gulp and scss files

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
 
 gulp.task( 'sass', function () {
-  return gulp.src( './sass/common.scss' )
+  return gulp.src( './sass/content-styles.scss' )
     .pipe( sass().on( 'error', sass.logError ) )
     .pipe( gulp.dest( './css/' ) );
 } );
