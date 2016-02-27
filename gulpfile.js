8090ca5a7616c16a697c99a60f4e7167a423ee39
miya0001 <miya@wpist.me>
2016-02-27 13:16:27 +0900

    add csslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
+var csslint = require( 'gulp-csslint' );
 
 gulp.task( 'sass', function () {
   return gulp.src( './sass/content-styles.scss' )
@@ -9,10 +10,21 @@ gulp.task( 'sass', function () {
     .pipe( gulp.dest( './css/' ) );
 } );
 
-gulp.task('watch', function () {
-    gulp.watch('sass/**/*.scss', ['sass']);
+gulp.task( 'watch', function () {
+    gulp.watch( 'sass/**/*.scss', ['sass'] );
 });
 
 gulp.task( 'default', [ 'sass' ], function () {
+
+} );
+
+
+gulp.task( 'lint', function() {
+  gulp.src( 'css/*.css' )
+    .pipe( csslint() )
+    .pipe( csslint.reporter() );
+} );
+
+gulp.task( 'test', [ 'lint' ], function () {
 
 } );
